7213218229a3ea60ab513f0aec56b3cb85d565c1
habiba_564@hotmail.com <habiba>
2017-10-03 11:55:08 +0300

    adding validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
 
+
 @NgModule({
   declarations: [
+    FormsModule,
     AppComponent,
     NavbarComponent,
     FormComponent
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -5,11 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent{
 
-  constructor() { }
-
-  ngOnInit() {
+  log(x) {
+    console.log(x);
   }
-
 }
